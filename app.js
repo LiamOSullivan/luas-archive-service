ef2e459fe6ab5a6b8eaeee3ec496e34af22766ac
LiamOSullivan <lmosulli@tcd.ie>
2019-10-04 22:11:26 +0100

    Parse list of Luas stop ids

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cron = require('node-cron');
-const util = require('util');
-const moment = require('moment');
-const fetch = require("node-fetch");
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cron = require('node-cron')
+const util = require('util')
+const moment = require('moment')
+const fetch = require("node-fetch")
+const fs = require('fs')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const csv = require('csv-parser');
+const HTMLParser = require('node-html-parser');
 
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
@@ -44,11 +46,93 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/*
+Parse the file list of stop ids to query later
+*/
+let stopIDs = [];
+let stopID;
+
+fs.createReadStream('data/luas-stops.txt')
+  .pipe(csv({
+    separator: '\t'
+  }))
+  .on('headers', (headers) => {
+    stopID = headers[0]; //hack: first header was being returned as a string?
+  })
+  .on('data', (row) => {
+    // console.log(`Stop ID: ${row[stopID]}`);
+    stopIDs.push(row[stopID]);
+  })
+  .on('end', () => {
+    console.log('File successfully processed');
+    console.log(stopIDs);
+  })
+  .on('error', (e) => {
+    console.error(`There's been an error ${e}`);
+  });
+
+
+
+
+let luasAPIBase = "https://luasforecasts.rpa.ie/analysis/view.aspx?id=";
+
+const getLuasHTML = async url => {
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    // console.log("\n******\nLuas Data: " + JSON.stringify(html) + "\n******\n");
+    return html;
+
+  } catch (error) {
+    return util.log(error);
+  }
+};
+
+const html = async () => {
+  const res = await getLuasHTML(luasAPIBase + '1');
+  return res;
+};
+
+
+// const htmlDoc = HTMLParser.parse(html);
+// console.log(htmlDoc);
+// console.log(htmlDoc.firstChild.structure);
+
+
+// console.log(getLuasHTML(luasAPIBase + '1'));
+
 
 cron.schedule('*/1 * * * *', () => {
-  util.log(`Running luas cron`);
+  // util.log(`Running luas cron`);
+  // const html = getLuasHTML(luasAPIBase + '1')
+  // const htmlDoc = HTMLParser.parse(luasAPIBase + '1');
+  // console.log(htmlDoc);
+  // console.log(htmlDoc.firstChild.structure);
 
-
+  // d3.html(luasAPIBase + '1')
+  //   .then(function(htmlDoc) {
+  //     //                console.log(htmlDoc.body);
+  //     let infoString = htmlDoc.getElementById("cplBody_lblMessage")
+  //       .childNodes[0].nodeValue;
+  //     //console.log("info: " + infoString + "\n");
+  //     let headings = htmlDoc.getElementsByTagName("th");
+  //     //console.log("#cols = " + headings.length + "\n");
+  //     let rows = htmlDoc.getElementsByTagName("tr");
+  //     //console.log("#rows = " + rows.length + "\n");
+  //     let tableData = [];
+  //     for (let i = 1; i < rows.length; i += 1) {
+  //       let obj = {};
+  //       for (let j = 0; j < headings.length; j += 1) {
+  //         let heading = headings[j]
+  //           .childNodes[0]
+  //           .nodeValue;
+  //         let value = rows[i].getElementsByTagName("td")[j].innerHTML;
+  //         //console.log("\nvalue: "+ value);
+  //         obj[heading] = value;
+  //       }
+  //       //console.log("\n");
+  //       tableData.push(obj);
+  //     }
 });
 
 module.exports = app;
