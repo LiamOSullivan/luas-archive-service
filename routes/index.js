02b2b8d34f6f2a0b975a01d65be35d70722e0658
LiamOSullivan <lmosulli@tcd.ie>
2019-10-04 18:43:04 +0100

    Add some utils

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+let d = new Date();
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Luas data archiving service'
+    title: 'Luas data archiving service ',
+    time: `${d}`
   });
 });
 
